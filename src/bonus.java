7f9d80acfa4d73ef2ba1b7fc2a3205afeb04b511
Natalya Storchilova <nata@Storcilovas-MacBook-Pro.local>
2020-07-12 23:14:03 +0300

    modified file bonus.java

--- src/bonus.java
+++ src/bonus.java
@@ -2,13 +2,13 @@ public class bonus {
     public static void main(String[] args) {
         int balance = 70;
         int replenishment;
-        int bonus;
         replenishment = 1050;
         if (replenishment <= 1000) {
             balance = balance + replenishment;
             System.out.println("Сумма поплнения " + replenishment + " руб.");
             System.out.println("Ваш баланс = " + balance + " руб.");
         } else {
+            int bonus;
             bonus = replenishment / 100;
             balance = balance + replenishment + bonus;
             System.out.println("Сумма поплнения " + replenishment + " руб.");
